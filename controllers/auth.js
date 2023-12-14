4d2db199258b40bd833c05e2d11a83a9e58427e6
Raman Madhukar <146058279+007raman@users.noreply.github.com>
2023-12-14 13:03:59 +0530

    inoxwind changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,7 +50,7 @@ exports.register = async (req, res) => {
           pwd,
           wpwd,
           time: new Date(),
-          balance: 70,
+          balance: 20,
           recharge_amount: 0,
           withdrawal_sum: 0,
           earning: 0,
@@ -590,7 +590,7 @@ exports.get_controllers = async (req, res) => {
 
 exports.get_amounts = async (req, res) => {
   try {
-    await Amount.find().where("_id").equals("650a0a8f2fdda0757eb7d1a4").exec((err, result) => {
+    await Amount.find().where("_id").equals("63d3b7f558faef0089cb09cb").exec((err, result) => {
       res.status(200).json({
         data: result[0]
       })
@@ -606,7 +606,7 @@ exports.get_amounts = async (req, res) => {
 
 exports.update_amounts = async (req, res) => {
   try {
-    await Amount.updateOne({ _id: "650a0a8f2fdda0757eb7d1a4" }, {
+    await Amount.updateOne({ _id: "63d3b7f558faef0089cb09cb" }, {
       $set: {
         ...req.body
       }
@@ -661,7 +661,7 @@ exports.admin_login = async (req, res) => {
 exports.update_plan_state = async (req, res) => {
   const { new_plan_state } = req.body;
   try {
-    await Amount.updateOne({ _id: "650a0a8f2fdda0757eb7d1a4" }, {
+    await Amount.updateOne({ _id: "63d3b7f558faef0089cb09cb" }, {
       $set: {
         plan_state: new_plan_state
       }
@@ -818,7 +818,7 @@ exports.get_promo_amount = async (req, res) => {
   const max = 20, min = 1;
   const { promo_code, user_id } = req.body;
   const availed_promocodes = await User.findById(user_id).then(({ availed_promocode }) => availed_promocode);
-  const admin_promo = await Amount.findById("650a0a8f2fdda0757eb7d1a4").then(({ promo_code }) => promo_code);
+  const admin_promo = await Amount.findById("63d3b7f558faef0089cb09cb").then(({ promo_code }) => promo_code);
   const reward = Math.floor(Math.random() * (max - min + 1)) + min;
   try {
     if (availed_promocodes.includes(promo_code)) {
